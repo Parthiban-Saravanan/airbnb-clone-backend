fe953106698896babf7fad0fe4c31f6ef0f38bc5
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-08-19 03:56:21 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 const generateToken = require('../utils/generateToken');
 
 // @desc    Register a new user
-// @route   POST /api/users/signup
+// @route   POST /api/users/register
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -34,6 +34,21 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Delete a user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    await user.remove();
+    res.json({ message: 'User removed' });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
 // @desc    Authenticate user & get token
 // @route   POST /api/users/login
 // @access  Public
@@ -55,17 +70,48 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc    Get user profile
-// @route   GET /api/users/profile
-// @access  Private
-const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
 
   if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+// @desc    Update user profile
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
     res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
     });
   } else {
     res.status(404);
@@ -75,6 +121,9 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 module.exports = {
   registerUser,
+  deleteUser,
   loginUser,
-  getUserProfile,
+  getUsers,
+  getUserById,
+  updateUser,
 };
