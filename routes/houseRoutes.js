3bb11fc471753f1755571b1ec71c365deeb92056
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-08-19 03:52:44 +0530

    Update houseRoutes.js

--- routes/houseRoutes.js
+++ routes/houseRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const { getAllHouses, createHouse, updateHouse, deleteHouse } = require('../controllers/houseController');
-const { protect } = require('../middleware/authMiddleware');
-
 const router = express.Router();
+const {
+  getAllHouses,
+  getHouseById,
+  createHouse,
+  updateHouse,
+  deleteHouse,
+} = require('../controllers/houseController');
+const { protect, admin } = require('../middleware/authMiddleware');
 
 router.get('/', getAllHouses);
-router.post('/', protect, createHouse);
-router.put('/:id', protect, updateHouse);
-router.delete('/:id', protect, deleteHouse);
+router.get('/:id', getHouseById);
+router.post('/', protect, admin, createHouse);
+router.put('/:id', protect, admin, updateHouse);
+router.delete('/:id', protect, admin, deleteHouse);
 
 module.exports = router;
