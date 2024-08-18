18dcf969e094682c5ed629bb18c44e3a24c7b5dc
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-08-19 03:55:11 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -10,6 +10,11 @@ const protect = asyncHandler(async (req, res, next) => {
       token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select('-password');
+
+      if (!req.user) {
+        throw new Error('User not found');
+      }
+
       next();
     } catch (error) {
       console.error(error);
@@ -24,4 +29,13 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-module.exports = { protect };
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an admin');
+  }
+};
+
+module.exports = { protect, admin };
