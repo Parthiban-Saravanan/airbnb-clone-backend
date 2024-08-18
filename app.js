d1aa806f1f3a9e611815ad93a8242118e3127d3b
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-08-19 05:21:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 // CORS middleware
 app.use(cors({
-    origin: 'https://illustrious-crisp-77e232.netlify.app/'
+    origin:'https://illustrious-crisp-77e232.netlify.app/'
   }));
   
 
