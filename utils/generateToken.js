b0d586b077370d90ecc2e620a682848210eb2ba8
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-09-03 05:21:16 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 const jwt = require('jsonwebtoken');
 
+// Function to generate a JWT token
 const generateToken = (userId) => {
     return jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: '30d' // Token expires in 30 days
