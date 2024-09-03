aa53c36efded583d1a8c1d1b5451f1d5d53c002b
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-09-03 06:09:38 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,13 +5,12 @@ const connectDB = async () => {
     const conn = await mongoose.connect('mongodb+srv://parthibanaakash78:o4tFtDleX4L3CBAE@airbnb.edvg3.mongodb.net/airbnb-data?retryWrites=true&w=majority&appName=airbnb', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
     });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log('MongoDB connected');
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Exit process with failure
   }
 };
 
