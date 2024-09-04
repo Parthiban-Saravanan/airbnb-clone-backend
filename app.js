99d4823f8778f2f59f533ee61d73ff1262f7d848
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-09-04 12:51:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,17 +19,12 @@ connectDB();
 // Middleware for parsing JSON
 app.use(express.json());
 
-//CORS Configuration
+// CORS configuration
 const corsOptions = {
-  origin: [
-    'https://main--illustrious-crisp-77e232.netlify.app',
-    'https://illustrious-crisp-77e232.netlify.app',
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: 'https://illustrious-crisp-77e232.netlify.app', // frontend URL
+  credentials: true // Allow credentials
 };
 
-// Apply CORS middleware
 app.use(cors(corsOptions));
 
 // Test route to ensure server is running
