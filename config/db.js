22f13e72c726496ba5752ea282be392fdd2cd472
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-09-09 01:49:15 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://parthibanaakash78:o4tFtDleX4L3CBAE@airbnb.edvg3.mongodb.net/airbnb-data?retryWrites=true&w=majority&appName=airbnb', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect('mongodb+srv://parthibanaakash78:o4tFtDleX4L3CBAE@airbnb.edvg3.mongodb.net/airbnb-data?retryWrites=true&w=majority&appName=airbnb');
 
     console.log('MongoDB connected');
   } catch (error) {
