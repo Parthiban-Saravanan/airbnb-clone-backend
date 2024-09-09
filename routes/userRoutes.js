65d18f7ba10f36c5223ce9293bc0e6ebd4fbb47d
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-09-09 06:27:40 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const router = express.Router();
 const { registerUser, loginUser, getUserProfile } = require('../controllers/userController');
-const { protect } = require('../middleware/authMiddleware'); // Ensure the correct import
+const { protect } = require('../middleware/authMiddleware');
+const router = express.Router();
 
-// Route for user registration
 router.post('/signup', registerUser);
-
-// Route for user login
 router.post('/login', loginUser);
-
-// Route for fetching user profile, protected by authMiddleware
-router.get('/profile', protect, getUserProfile); // Ensure protect is used as middleware
+router.get('/profile', protect, getUserProfile);
 
 module.exports = router;
