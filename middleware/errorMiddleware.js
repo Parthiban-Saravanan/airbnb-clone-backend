be799062919ac739bbbf8c92759d8d5e87191856
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-09-09 06:24:53 +0530

    Update errorMiddleware.js

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,11 +1,10 @@
 const errorMiddleware = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
+  
+  res.status(statusCode).json({
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-    error: err,
   });
 };
 
-module.exports = { errorMiddleware };
+module.exports = errorMiddleware;
