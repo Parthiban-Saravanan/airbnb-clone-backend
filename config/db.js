462921f095959d4001866da1a3cf8636260a4d76
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-09-09 06:22:20 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://parthibanaakash78:o4tFtDleX4L3CBAE@airbnb.edvg3.mongodb.net/airbnb-data?retryWrites=true&w=majority&appName=airbnb');
-
-    console.log('MongoDB connected');
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error(`Error: ${error.message}`);
     process.exit(1); // Exit process with failure
   }
 };
