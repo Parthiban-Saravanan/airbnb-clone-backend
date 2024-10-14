51c9c754859df1f8454655d7cd3b8c44b88d2a64
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-10-14 11:24:48 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
 const User = require('../models/User');
 
-const protect = async (req, res, next) => {
+const protect = asyncHandler(async (req, res, next) => {
   let token;
 
   if (
@@ -10,25 +11,24 @@ const protect = async (req, res, next) => {
   ) {
     try {
       token = req.headers.authorization.split(' ')[1];
+
+      // Decode token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Find the user by ID stored in token
       req.user = await User.findById(decoded.id).select('-password');
+
       next();
     } catch (error) {
-      res.status(401).json({ message: 'Not authorized, token failed' });
+      res.status(401);
+      throw new Error('Not authorized, token failed');
     }
   }
 
   if (!token) {
-    return res.status(401).json({ message: 'Not authorized, no token' });
+    res.status(401);
+    throw new Error('Not authorized, no token');
   }
-};
+});
 
-const admin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
-    next();
-  } else {
-    res.status(403).json({ message: 'Not authorized as an admin' });
-  }
-};
-
-module.exports = { protect, admin };
+module.exports = { protect };
