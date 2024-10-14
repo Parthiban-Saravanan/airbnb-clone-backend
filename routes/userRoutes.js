9838bc6a0f14d68bfe37199159e024963f0d6f07
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-10-14 12:20:35 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const {
-  registerUser,
-  authUser,
+  signup,
+  login,
   getUserProfile,
 } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 // Public Routes
-router.post('/signup', registerUser);
-router.post('/login', authUser);
+router.post('/signup', signup);
+router.post('/login', login);
 
 // Private Route (Protected with token)
 router.get('/profile', protect, getUserProfile);
