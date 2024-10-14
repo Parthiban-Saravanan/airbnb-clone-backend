6c3036bcd990ad23a69b324929ea3a25817a7f4e
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-10-14 08:25:24 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {
+  name: {
     type: String,
     required: true,
-    unique: true,
   },
   email: {
     type: String,
@@ -15,6 +14,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-}, { timestamps: true });
+});
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User ', userSchema);
